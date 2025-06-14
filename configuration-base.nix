322c419b56b2e8fda89684e4fb2eb3d80bd46947
bikingalong <bikingalong@pm.me>
2025-06-13 23:06:45 -0500

    Remove console mode declaration

--- configuration-base.nix
+++ configuration-base.nix
@@ -11,7 +11,6 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.consoleMode = "auto";
   # Boot kernel parameters:
   boot.kernelParams = [
     # Mitigate screen flickering, see:
