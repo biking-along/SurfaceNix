bd04b9c2d28d1aa49ac9d9514103462ba76c50d3
bikingalong <bikingalong@pm.me>
2025-06-13 22:53:37 -0500

    Change systemd boot console mode

--- configuration-base.nix
+++ configuration-base.nix
@@ -11,6 +11,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.consoleMode = max;
   # Boot kernel parameters:
   boot.kernelParams = [
     # Mitigate screen flickering, see:
