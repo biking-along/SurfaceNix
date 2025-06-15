84ada97b5ffa720d2900c8ba2ba1ad14a83e140a
bikingalong <bikingalong@pm.me>
2025-06-15 12:53:47 -0500

    Separate hiding desktop entries to it's own module

--- hosts/kappa/configuration.nix
+++ hosts/kappa/configuration.nix
@@ -16,26 +16,15 @@
     ../../modules/kappa/gnome/gnome.nix
     ../../modules/kappa/cachix/cachix.nix
     ../../modules/kappa/envVars.nix
+    ../../modules/shared/hideDesktopEntry.nix
   ];
 
-  # enable core dumps.
   systemd.coredump.enable = true;
 
-  networking.hostName = "kappa"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "kappa";
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
 
   boot.tmp.cleanOnBoot = true;
   hardware.enableAllFirmware = true;
@@ -77,22 +66,8 @@
     };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = (with pkgs; [
     neovim
-    # Hide neovim wrapper desktop entry
-    (lib.hiPrio (pkgs.runCommand "nvim.desktop-hide" { } ''
-      mkdir -p "$out/share/applications"
-      cat "${config.programs.neovim.package}/share/applications/nvim.desktop" > "$out/share/applications/nvim.desktop"
-      echo "Hidden=1" >> "$out/share/applications/nvim.desktop"
-    ''))
-    # Hide fish desktop entry
-    (lib.hiPrio (pkgs.runCommand "fish.desktop-hide" { } ''
-       mkdir -p "$out/share/applications"
-       cat "${config.programs.fish.package}/share/applications/fish.desktop" > "$out/share/applications/fish.desktop"
-       echo "Hidden=1" >> "$out/share/applications/fish.desktop"
-     ''))
     gawk
     wget
     iftop
@@ -124,37 +99,8 @@
     '')
   ];
 
-  networking.wireless.enable =
-    false; # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable =
-    true; # Easiest to use and most distros use this by default.
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable mosh, and open firewall as appropriate.
-  # programs.mosh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  networking.wireless.enable = false;
+  networking.networkmanager.enable = true;
 
   documentation.nixos.enable = false;
 
